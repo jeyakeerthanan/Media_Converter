7af3763409f467322c5e1073389b19fd91a6f4c5
Ahrur <ahrur12@gmail.com>
2020-07-16 16:16:47 +0530

    Conversion to mp4 to wav

---
+++ src/com/company/MP4toWAVconverter.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import it.sauronsoftware.jave.*;
+
+public class MP4toWAVconverter extends Converter{
+
+    File Audio = new File(Main.l_path);
+
+    @Override
+    void convert()throws IllegalArgumentException,EncoderException {
+
+        File target = new File(Main.l_path);
+        AudioAttributes audio = new AudioAttributes();
+        audio.setCodec("libmp3lame");
+        audio.setBitRate(new Integer(128000));
+        audio.setChannels(new Integer(2));
+        audio.setSamplingRate(new Integer(44100));
+        EncodingAttributes attrs = new EncodingAttributes();
+        attrs.setFormat("wav");
+        attrs.setAudioAttributes(audio);
+        Encoder encoder = new Encoder();
+        //encoder.encode(vid, target, attrs);
+        try {
+            encoder.encode(Audio, target, attrs, null);
+            System.out.println("Successfully Converted!! mp3 file is opening.....");
+            File file = new File (Main.l_path);
+            Desktop desktop = Desktop.getDesktop();
+            desktop.open(file);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        catch (InputFormatException e) {
+            e.printStackTrace();
+        }
+        catch (EncoderException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
