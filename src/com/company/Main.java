cd14f0341938a0edbc9da596db6400a61d0c8cab
jk <54949016+jeyakeerthanan@users.noreply.github.com>
2020-07-16 10:35:49 +0530

    get the input from the user

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Check the git");
+      //input the path of the file that need to convert
+        System.out.println("Enter the path of the file that you want to convert?");
+        Scanner ins=new Scanner(System.in);
+        String path=ins.nextLine();
+
+
+        //get the format that the user want to format
+        System.out.println("Enter the converting format!");
+        System.out.println("[mp4 to mp3]-press 1 or [mp4 to wav]-press 2");
+        Scanner format=new Scanner(System.in);
+        int num=format.nextInt();
+
     }
 }
