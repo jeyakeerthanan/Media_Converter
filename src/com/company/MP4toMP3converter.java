73e5fc67122d42f030840f9e098e9a7c46527eee
jk <54949016+jeyakeerthanan@users.noreply.github.com>
2020-07-16 14:02:24 +0530

    open converted file

--- src/com/company/MP4toMP3converter.java
+++ src/com/company/MP4toMP3converter.java
@@ -2,6 +2,7 @@ package com.company;
 
 import it.sauronsoftware.jave.*;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -25,7 +26,25 @@ public class MP4toMP3converter extends Converter {
         attrs.setFormat("mp3");
         attrs.setAudioAttributes(audio);
         Encoder encoder = new Encoder();
-        encoder.encode(vid, target, attrs);
+        //encoder.encode(vid, target, attrs);
+        try {
+            encoder.encode(vid, target, attrs, null);
+            System.out.println("Successfully Converted!! mp3 file is opening.....");
+            File file = new File (Main.l_path);
+            Desktop desktop = Desktop.getDesktop();
+            desktop.open(file);
+        } catch (IllegalArgumentException e) {
+
+            e.printStackTrace();
+        } catch (InputFormatException e) {
+
+            e.printStackTrace();
+        } catch (EncoderException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
