8afb0e0d3edc05b4d0a8ca5b428b49396ae65e69
jk <54949016+jeyakeerthanan@users.noreply.github.com>
2020-07-16 10:44:12 +0530

    get the saving location

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+    public static String l_path;
     public static void main(String[] args) {
       //input the path of the file that need to convert
         System.out.println("Enter the path of the file that you want to convert?");
@@ -16,6 +16,18 @@ public class Main {
         System.out.println("[mp4 to mp3]-press 1 or [mp4 to wav]-press 2");
         Scanner format=new Scanner(System.in);
         int num=format.nextInt();
+
+        // change the location of the converted file
+        System.out.println("If you want to change the location  of the converted file press 5 otherwise press 6 it will be store on D:\\convert/Mp4_To_Mp3.mp3");
+        Scanner location=new Scanner(System.in);
+        int Location_change=location.nextInt();
+        if (Location_change==5){
+            System.out.println("please enter the location path ......");
+            Scanner l_ans=new Scanner(System.in);
+            l_path=l_ans.nextLine();
+        }
+        else if (Location_change==5)
+            l_path="D:\\converted.mp3";
 
     }
 }
